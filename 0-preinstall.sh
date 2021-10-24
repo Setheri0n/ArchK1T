eec81e1647a83a9e564e43ba277d6caffe1c2628
Setheri0n <82003785+Setheri0n@users.noreply.github.com>
2021-10-23 19:58:34 -0700

    updates and changes

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -29,7 +29,7 @@ reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt
 
 echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm gptfdisk btrfs-progs grub-btrfs grub
+pacman -S --noconfirm gptfdisk btrfs-progs
 
 echo "-------------------------------------------------"
 echo "         select your disk to format              "
@@ -41,61 +41,61 @@ echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
 read -p "are you sure you want to continue (Y/N):" formatdisk
 case $formatdisk in
 
-y|Y|yes|Yes|YES)
+y|YES)
 echo "--------------------------------------"
 echo -e "\nFormatting disk...\n$HR"
 echo "--------------------------------------"
 
 # disk prep
-sgdisk -Z ${DISK} # zap all on disk
-dd if=/dev/zero of=${DISK} bs=1M count=200 conv=fdatasync status=progress
+sgdisk -Z ${DISK} # zap all on drive
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1:0:+300M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+sgdisk -n 1:0:301M ${DISK} # partition 1 (EFI), default start block, 300MB
+sgdisk -n 2:0:0 ${DISK} # partition 2 (Root), default start, remaining
 
 # set partition types
 sgdisk -t 1:ef00 ${DISK}
 sgdisk -t 2:8300 ${DISK}
 
 # label partitions
-sgdisk -c 1:"UEFISYS" ${DISK}
+sgdisk -c 1:"EFI" ${DISK}
 sgdisk -c 2:"ROOT" ${DISK}
 
 # make filesystems
 echo -e "\nCreating Filesystems...\n$HR"
 if [[ ${DISK} =~ "nvme" ]]; then
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}p1"
+mkfs.vfat -F32 -n "EFI" "${DISK}p1"
 mkfs.btrfs -L "ROOT" "${DISK}p2" -f
 mount -t btrfs "${DISK}p2" /mnt
 else
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.vfat -F32 -n "EFI" "${DISK}1"
 mkfs.btrfs -L "ROOT" "${DISK}2" -f
 mount -t btrfs "${DISK}2" /mnt
 fi
-ls /mnt | xargs btrfs subvolume delete
-btrfs subvolume create /mnt/@
+cd /mnt
+btrfs subvolume create /@
 umount /mnt
+cd..
 ;;
 esac
 
-# mount target
-mount -t btrfs -o subvol=@ -L ROOT /mnt
-mkdir /mnt/boot
+# mount target /
+mount -o subvol=/@ -L ROOT /mnt
+mkdir /mnt/
 mkdir /mnt/boot/efi
-mount -t vfat -L UEFISYS /mnt/boot/
+mount "{DISK}2" /mnt/boot/efi
 
 echo "--------------------------------------"
 echo "     Arch Install on Main Drive       "
 echo "--------------------------------------"
-pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
 echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 echo "--------------------------------------"
 echo "   Bootloader Systemd Installation    "
 echo "--------------------------------------"
-bootctl install --esp-path=/mnt/boot/efi 
+bootctl install --esp-path=/
 [ ! -d "/mnt/boot/loader/entries" ] && mkdir -p /mnt/boot/loader/entries
 cat <<EOF > /mnt/boot/loader/entries/arch.conf
 title Arch Linux  
