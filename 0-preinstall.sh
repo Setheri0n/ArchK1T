c28041766a5838dfe3950d7701c1ede650f9c519
Setheri0n <82003785+Setheri0n@users.noreply.github.com>
2021-10-26 03:38:25 -0700

    updates and cleanup

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -6,25 +6,25 @@
 # |_|_||_|  \_|_.|_|_||_\_\|_| |_|  #
 #                                   #
 #-----------------------------------#
-
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "-------------------------------------------------"
 echo "     Setting up mirrors for optimal download     "
 echo "-------------------------------------------------"
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
-pacman -Sy pacman-contrib terminus-font --noconfirm --needed
+pacman -S --noconfirm pacman-contrib terminus-font
 setfont ter-v22b
 sed -i 's/^#Para/Para/' /etc/pacman.conf
-pacman -S reflector rsync --noconfirm --needed
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo -e "-----------------------------------"
-echo -e "  ___            _    _ __ _  ___  "
-echo -e " | . | _ _  ___ | |_ | / // ||_ _| "
-echo -e " |   || '_>/ | '| . ||  \ | | | |  "
-echo -e " |_|_||_|  \_|_.|_|_||_\_\|_| |_|  "
-echo -e "   Waiting to select Hard disk     "
-echo -e "        Please standby........     "
-echo -e "-----------------------------------"
+pacman -S --noconfirm reflector rsync
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+echo "-----------------------------------"
+echo "  ___            _    _ __ _  ___  "
+echo " | . | _ _  ___ | |_ | / // ||_ _| "
+echo " |   || '_>/ | '| . ||  \ | | | |  "
+echo " |_|_||_|  \_|_.|_|_||_\_\|_| |_|  "
+echo "   Waiting to select Hard disk     "
+echo "        Please standby........     "
+echo "-----------------------------------"
 reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt
 
@@ -47,29 +47,29 @@ echo -e "\nFormatting disk...\n$HR"
 echo "--------------------------------------"
 
 # disk prep
-sgdisk -Z ${DISK} # zap all on drive
+sgdisk -Z ${DISK} # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1:0:301M ${DISK} # partition 1 (EFI), default start block, 300MB
-sgdisk -n 2:0:0 ${DISK} # partition 2 (Root), default start, remaining
+sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
+sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
 
 # set partition types
 sgdisk -t 1:ef00 ${DISK}
 sgdisk -t 2:8300 ${DISK}
 
 # label partitions
-sgdisk -c 1:"EFI" ${DISK}
+sgdisk -c 1:"UEFISYS" ${DISK}
 sgdisk -c 2:"ROOT" ${DISK}
 
 # make filesystems
 echo -e "\nCreating Filesystems...\n$HR"
 if [[ ${DISK} =~ "nvme" ]]; then
-mkfs.vfat -F32 -n "EFI" "${DISK}p1"
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}p1"
 mkfs.btrfs -L "ROOT" "${DISK}p2" -f
 mount -t btrfs "${DISK}p2" /mnt
 else
-mkfs.vfat -F32 -n "EFI" "${DISK}1"
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
 mkfs.btrfs -L "ROOT" "${DISK}2" -f
 mount -t btrfs "${DISK}2" /mnt
 fi
@@ -77,13 +77,27 @@ ls /mnt | xargs btrfs subvolume delete
 btrfs subvolume create /mnt/@
 umount /mnt
 ;;
+*)
+echo "Rebooting in 3 Seconds ..." && sleep 1
+echo "Rebooting in 2 Seconds ..." && sleep 1
+echo "Rebooting in 1 Second ..." && sleep 1
+reboot now
+;;
 esac
 
 # mount target
 mount -t btrfs -o subvol=@ -L ROOT /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
-mount -t vfat -L EFI /mnt/boot/
+mount -t vfat -L UEFISYS /mnt/boot/
+
+if ! grep -qs '/mnt' /proc/mounts; then
+    echo "Drive is not mounted can not continue"
+    echo "Rebooting in 3 Seconds ..." && sleep 1
+    echo "Rebooting in 2 Seconds ..." && sleep 1
+    echo "Rebooting in 1 Second ..." && sleep 1
+    reboot now
+fi
 
 echo "--------------------------------------"
 echo "     Arch Install on Main Drive       "
@@ -106,6 +120,16 @@ options root=LABEL=ROOT rw rootflags=subvol=@
 EOF
 cp -R ~/ArchK1T /mnt/root/
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+echo "--------------------------------------"
+echo "  Checking for low memory systems ~8G "
+echo "--------------------------------------"
+TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTALMEM -lt 8000000 ]]; then
+    dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+fi
 echo "--------------------------------------"
 echo "     SYSTEM READY FOR 1-setup         "
 echo "--------------------------------------"
