662f238b59ec7edbaebe6afe703c84a490438549
Setheri0n <82003785+Setheri0n@users.noreply.github.com>
2021-10-26 04:19:04 -0700

    Update 0-preinstall.sh
    resolve symlinks in $BASH_SOURCE

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -6,7 +6,18 @@
 # |_|_||_|  \_|_.|_|_||_\_\|_| |_|  #
 #                                   #
 #-----------------------------------#
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
 echo "-------------------------------------------------"
 echo "     Setting up mirrors for optimal download     "
 echo "-------------------------------------------------"
@@ -130,6 +141,3 @@ if [[  $TOTALMEM -lt 8000000 ]]; then
     mkswap /swapfile
     swapon /swapfile
 fi
-echo "--------------------------------------"
-echo "     SYSTEM READY FOR 1-setup         "
-echo "--------------------------------------"
